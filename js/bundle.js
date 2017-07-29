0b5897a5a7437227bdf449ef5739aaf4b09374fb
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-07-29 17:39:12 +0100

    Add eye and ground

--- js/bundle.js
+++ js/bundle.js
@@ -29,6 +29,8 @@ var PreloaderScene = {
     this.game.load.image('circuit', 'images/circuit.png');
     this.game.load.image('connector', 'images/connector.png');
     this.game.load.image('light', 'images/light.png');
+    this.game.load.image('ground', 'images/ground.png');
+    this.game.load.spritesheet('eye', 'images/eye.png', 32, 48);
   },
 
   create: function () {
@@ -51,17 +53,36 @@ window.onload = function () {
 'use strict';
 
 var PlayScene = {
+  init: function (levelIndex) {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  },
+
   create: function () {
-    var circuit;
+    var circuit, ground;
 
     this.game.add.sprite(0, 0, 'background:level1');
-    this.game.add.sprite(380, 475, 'button');
+    this.game.add.sprite(380, this.game.world.height - 100 - 30, 'button');
     this.game.add.sprite(583, 180, 'light');
     this.game.add.sprite(583, 430, 'connector');
 
     // circuit = this.game.add.sprite(393, 198, 'circuit');
     circuit = this.game.add.sprite(393, 200, 'circuit');
     circuit.angle -= 45;
+
+    this.platforms = this.game.add.group();
+    this.platforms.enableBody = true;
+    ground = this.platforms.create(0, this.game.world.height - 100, 'ground');
+    ground.body.immovable = true;
+
+    this.eye = this.game.add.sprite(32, this.game.world.height - 150, 'eye'),
+    this.game.physics.arcade.enable(this.eye);
+    this.eye.body.bounce.y = 0.2;
+    this.eye.body.gravity.y = 300;
+    this.eye.body.collideWorldBounds = true;
+  },
+
+  update: function () {
+    this.game.physics.arcade.collide(this.eye, this.platforms);
   }
 };
 
