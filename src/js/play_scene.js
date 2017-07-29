3b1ecdab2e7d354c6ca0c872ee353155c26dffb7
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-07-29 19:09:09 +0100

    Allow pressing the button to close the circuit

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -4,24 +4,23 @@ var PlayScene = {
   init: function (levelIndex) {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.keys = this.game.input.keyboard.createCursorKeys();
+    this.groundPosition = this.game.world.height - 100;
+    this.buttonPosition = this.groundPosition - 30
   },
 
   create: function () {
-    var circuit;
-
     this.game.add.sprite(0, 0, 'background:level1');
     this.game.add.sprite(583, 180, 'light');
     this.game.add.sprite(583, 430, 'connector');
 
-    // circuit = this.game.add.sprite(393, 198, 'circuit');
-    circuit = this.game.add.sprite(393, 200, 'circuit');
-    circuit.angle -= 45;
+    this.circuit = this.game.add.sprite(393, 200, 'circuit');
+    this.circuit.angle = -45;
 
-    this.ground = this.game.add.sprite(0, this.game.world.height - 100, 'ground');
+    this.ground = this.game.add.sprite(0, this.groundPosition, 'ground');
     this.game.physics.arcade.enable(this.ground);
     this.ground.body.immovable = true;
 
-    this.button = this.game.add.sprite(380, this.game.world.height - 100 - 30, 'button');
+    this.button = this.game.add.sprite(380, this.buttonPosition, 'button');
     this.game.physics.arcade.enable(this.button);
     this.button.body.immovable = true;
 
@@ -53,6 +52,13 @@ var PlayScene = {
 
     if (this.keys.up.isDown && this.eye.body.touching.down && hitGround) {
       this.eye.body.velocity.y = -600;
+    }
+
+    if (hitButton && this.button.body.touching.up) {
+      this.button.y += this.button.height;
+      this.circuit.x = 393;
+      this.circuit.y = 198;
+      this.circuit.angle = 0;
     }
   }
 };
