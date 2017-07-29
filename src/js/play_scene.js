4401741ebaecae551b7dcf14effd582476431209
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-07-29 22:26:52 +0100

    Extract eye into it's own class

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Eye = require('./sprites/eye.js');
+
 var PlayScene = {
   init: function (levelIndex) {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -24,35 +26,15 @@ var PlayScene = {
     this.game.physics.arcade.enable(this.button);
     this.button.body.immovable = true;
 
-    this.eye = this.game.add.sprite(32, this.game.world.height - 150, 'eye'),
-    this.game.physics.arcade.enable(this.eye);
-    this.eye.body.bounce.y = 0.2;
-    this.eye.body.gravity.y = 700;
-    this.eye.body.collideWorldBounds = true;
-    this.eye.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.eye.animations.add('right', [5, 6, 7, 8], 10, true);
+    this.eye = new Eye(this.game, 32, this.game.world.height - 150, this.keys);
+    this.game.add.existing(this.eye);
   },
 
   update: function () {
     var hitGround = this.game.physics.arcade.collide(this.eye, this.ground);
     var hitButton = this.game.physics.arcade.collide(this.eye, this.button);
 
-    this.eye.body.velocity.x = 0;
-
-    if (this.keys.left.isDown) {
-      this.eye.body.velocity.x = -300;
-      this.eye.animations.play('left');
-    } else if (this.keys.right.isDown) {
-      this.eye.body.velocity.x = 300;
-      this.eye.animations.play('right');
-    } else {
-      this.eye.animations.stop();
-      this.eye.frame = 4;
-    }
-
-    if (this.keys.up.isDown && this.eye.body.touching.down && hitGround) {
-      this.eye.body.velocity.y = -600;
-    }
+    this.eye.update(hitGround);
 
     if (hitButton && this.button.body.touching.up) {
       this.button.y += this.button.height;
---
+++ src/js/sprites/eye.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function Eye(game, x, y, keys) {
+  Phaser.Sprite.call(this, game, x, y, 'eye');
+  this.keys = keys;
+
+  this.game.physics.arcade.enable(this);
+  this.body.setSize(32, 48);
+  this.body.bounce.y = 0.2;
+  this.body.gravity.y = 700;
+  this.body.collideWorldBounds = true;
+
+  this.animations.add('left', [0, 1, 2, 3], 10, true);
+  this.animations.add('right', [5, 6, 7, 8], 10, true);
+};
+
+Eye.prototype = Object.create(Phaser.Sprite.prototype);
+Eye.prototype.constructor = Eye;
+
+Eye.prototype.update = function(hitGround) {
+  this.body.velocity.x = 0;
+  this._move();
+  this._jump(hitGround);
+};
+
+Eye.prototype._move = function() {
+  if (this.keys.left.isDown) {
+    this.body.velocity.x = -300;
+    this.animations.play('left');
+  } else if (this.keys.right.isDown) {
+    this.body.velocity.x = 300;
+    this.animations.play('right');
+  } else {
+    this.animations.stop();
+    this.frame = 4;
+  }
+};
+
+Eye.prototype._jump = function(hitGround) {
+  if (this.keys.up.isDown && this.body.touching.down && hitGround) {
+    this.body.velocity.y = -600;
+  }
+};
+
+module.exports = Eye;
