86345e92a0e4379ba02451cc2ca4cb2f1b45cdae
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-07-30 02:03:44 +0100

    Refactor the code

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -12,7 +12,6 @@ PlayScene = {
   init: function (levelIndex) {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.keys = this.game.input.keyboard.createCursorKeys();
-    this.groundPosition = this.game.world.height - 100;
   },
 
   create: function () {
@@ -25,11 +24,11 @@ PlayScene = {
     this.circuit = new Circuit(this.game, 393, 200);
     this.game.add.existing(this.circuit);
 
-    this.ground = this.game.add.sprite(0, this.groundPosition, 'ground');
+    this.ground = this.game.add.sprite(0, this.game.world.height - 100, 'ground');
     this.game.physics.arcade.enable(this.ground);
     this.ground.body.immovable = true;
 
-    this.button = new Button(this.game, 380, this.groundPosition - 30);
+    this.button = new Button(this.game, 380, this.game.world.height - 100 - 30);
     this.game.add.existing(this.button);
 
     this.eye = new Eye(this.game, 32, this.game.world.height - 150, this.keys);
@@ -37,9 +36,11 @@ PlayScene = {
   },
 
   update: function () {
-    var hitGround = this.game.physics.arcade.collide(this.eye, this.ground);
-    var hitButton = this.game.physics.arcade.collide(this.eye, this.button);
-    var hitCharger = this.game.physics.arcade.collide(this.eye, this.charger);
+    var
+      hitGround = this.game.physics.arcade.collide(this.eye, this.ground),
+      hitButton = this.game.physics.arcade.collide(this.eye, this.button),
+      hitCharger = this.game.physics.arcade.collide(this.eye, this.charger)
+    ;
 
     this.eye.update(hitGround);
 
